415f276c7c13d88ad2edb87baa2ff63d1d21d968
Abhishek Singh Bhadauria <107433488+Abhish1sb@users.noreply.github.com>
2022-07-18 00:26:51 +0530

    Add files via upload

---
+++ bank/README.md
@@ -0,0 +1,3 @@
+# Basic Banking System
+Basic banking system website using html css bootstrap for front end, Php for backend and phpmyadmin sql for database.
+Website hosted using 000webhost at dbw07.000webhostapp.com
---
+++ bank/index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>TSF Bank</title>
+   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<!-- Navigation bar-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><h1> TSF Bank </h1></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php"> Home </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="customers.php"> Our Customers </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="transactions.php"> Transfer History </a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="https://www.oanda.com/currency-converter/en/"> Currency Convertor </a>
+        </li>
+        
+</div>
+
+
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+
+<!-- Caraousel-->
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+    <li data-target="#demo" data-slide-to="3"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+  <div class="carousel-item">
+      <img src="images/7.jpeg" alt="Customer Satisfaction" width="1100" height="500">
+      <div class="carousel-caption">
+    <h3>Low Interest Loans</h3>
+    </div>
+    </div>   
+    <div class="carousel-item">
+      <img src="images/4.jpeg" alt="Quick Loans" width="1100" height="500">
+      <div class="carousel-caption  d-none d-md-block">
+    <h3>Quick Loans</h3>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/6.jpeg" alt="Customer Satisfaction" width="1100" height="500">
+      <div class="carousel-caption">
+    <h3>Customer Satisfaction</h3>
+    </div>
+    </div>
+
+    <div class="carousel-item active">
+      <img src="images/5.jpeg" alt="Easy Transfer" width="1100" height="500">
+      <div class="carousel-caption">
+    <h3>Easy Money Transfer</h3>
+    </div>
+    </div>
+  </div>
+  
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<!--About us-->
+
+<section class= "my-5">
+<div class="py-5">
+ <h3 class="text-center" >About Us</h3>
+ </div>
+
+<div class="container-fluid">
+ <div class ="row">
+<div  class="col-lg-6 col-md-6 col-12">
+      <img src="images/image.jpeg" class="img-fluid" height="20px">
+      </div>
+      <div  class="col-lg-6 col-md-6 col-12">
+      <h1>TSF Bank</h1>
+      <p1 > TSF Bank is a project made by Web development Intern Abhishek Singh Bhadauria. It is one of top private banks and as of today, TSF Bank has a banking network of "xyz" branches and "abc" ATM's in "pqr" cities/towns. TSF Bank offers a diverse range of financial products and banking services to customers through a growing branch and ATM network and digital channels such as Netbanking, Phonebanking and MobileBanking.
+      <br>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, iste facilis corrupti magni nulla architecto explicabo fugiat nihil, rerum ratione, omnis accusantium incidunt odit nostrum dignissimos praesentium beatae cumque repellat ab exercitationem libero ad? Maiores ducimus quidem id nulla sequi illum, odio molestias nisi! Optio reiciendis commodi ipsum nam vero iste, quo saepe libero itaque dolorum fugit voluptates quidem tenetur ea illum fuga ex! Nobis obcaecati cum provident ipsa eligendi. In temporibus odio suscipit ad ut at harum voluptatem maiores sint similique fuga neque itaque, quisquam corrupti, magni est voluptas facilis perspiciatis soluta, eligendi alias maxime. Dolor, a veritatis consectetur vitae eius libero distinctio iste odit, omnis sapiente consequuntur necessitatibus rem eveniet temporibus expedita voluptatibus tempora at numquam in corrupti esse. Eveniet delectus, provident quidem quaerat harum architecto repellat, nihil blanditiis voluptas nemo enim asperiores consequuntur dolorem suscipit. Molestias magnam quod eligendi sed. Possimus consequatur numquam iure architecto suscipit tempora!
+        
+        </div>  
+      
+
+ </div>
+ </div>
+
+<!--Dsiclaimer-->
+
+</section>
+<section class= "my-5">
+<div class="py-5">
+ <h3 class="text-center" >Disclaimer</h3>
+ <p1>This website is the
+      outcome of a intern project, and does not
+      necessarily represent the views of any organisation or any other individuals referenced or
+      acknowledged within the website. Anyone may reproduce, distribute, adapt, translate the content on the website, without explicit permission, provided that the content is accompanied by an acknowledgement that TSF Bank website is the source and that it is clearly indicated if changes were made to the original content.</p1>
+ </div>
+
+
+
+<footer class="text-center mt-2 py-5">
+        <p>&copy 2022  <b>Abhishek Singh Bhadauria</b> </br>Web Developer Intern <br>TSF Bank</p>
+      </footer>
+
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>  
+</body>
+</html>
